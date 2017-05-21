3a32521b34a5a5470165785958feb4399a390909
Zero AR <durmon.albasir@gmail.com>
2017-05-20 21:25:43 -0400

    resume a previous state; use checkbox; add gitignore

--- pythonpidoor/main.py
+++ pythonpidoor/main.py
@@ -2,23 +2,20 @@ import web
 import RPi.GPIO as GPIO 
 from web import form
 
+pin = 21
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(29, GPIO.OUT)
+GPIO.setup(pin, GPIO.OUT)
 
 #Defining the index page
 urls = ('/', 'index')
 render = web.template.render('templates') #index.html is stored in '/templates' folder
 app = web.application(urls, globals())
 
-""" Defining the buttons. 
-	id: 	id of element 
-	value: 	button value as interpreted by Python
-	html: 	text displayed in HTML page. 
-	class_: is HTML class
-	"""
 my_form = form.Form(
- form.Checkbox("toggleCtrl", id="toggleCtrl", value="YES", class_="on"),
+	form.Checkbox("toggleCtrl", id="toggleCtrl", value="YES", class_="on", checked="checked")
 )
+
 # Above, we actually need a toggle (or checkbox), not a button
 # This would read its value from a file, and write to the same file
 
@@ -26,10 +23,15 @@ my_form = form.Form(
 class index:
     # rendering the HTML page
     def GET(self):
-        form = my_form()
+	if GPIO.input(pin) == 0:
+		element = form.Checkbox("toggleCtrl", id="toggleCtrl", value="YES", class_="on", checked="checked")
+	else:
+		element = form.Checkbox("toggleCtrl", id="toggleCtrl", value="YES", class_="on")
+
+	my_form = form.Form(element)
 	title = "Python Pi Garage Door"
 
-        return render.index(form, title)
+        return render.index(my_form, title)
 
     # posting the data from the webpage to Pi
     def POST(self):
@@ -37,14 +39,14 @@ class index:
         form_data = web.input()
 	
 	if form_data.has_key("toggleCtrl"):
-		GPIO.output(29, False)
+		GPIO.output(pin, False)
 		print "Open"
 
-	elif !form_data.has_key("toggleCtrl"):
-		GPIO.output(29, True)
+	elif not form_data.has_key("toggleCtrl"):
+		GPIO.output(pin, True)
 		print "Close"
 		
-        raise web.seeother('/')
+        #raise web.seeother('/')
 # run
 if __name__ == '__main__':
     app.run()
